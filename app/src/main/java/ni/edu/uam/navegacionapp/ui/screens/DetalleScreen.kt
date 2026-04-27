5f1cde11e169baaf708664619113f2adb4535fba
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-04-27 17:26:02 -0600

    feat(detalle): mostrar información del usuario recibida por navegación

--- app/src/main/java/ni/edu/uam/navegacionapp/ui/screens/DetalleScreen.kt
+++ app/src/main/java/ni/edu/uam/navegacionapp/ui/screens/DetalleScreen.kt
@@ -1,2 +1,50 @@
 package ni.edu.uam.navegacionapp.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun DetalleScreen(navController: NavController, nombre: String) {
+
+    val nombreSeguro = nombre.ifBlank { "Usuario" }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text("Detalle", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text("Hola, $nombreSeguro ")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("perfil") {
+                    launchSingleTop = true
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ir a Perfil")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedButton(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Volver")
+        }
+    }
+}
