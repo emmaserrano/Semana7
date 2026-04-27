e4e126f97038f4cc6d76277a181fa08f3857002d
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-04-27 17:27:59 -0600

    feat(perfil): implementar sistema de likes con límite y control de decremento

--- app/src/main/java/ni/edu/uam/navegacionapp/ui/screens/PerfilScreen.kt
+++ app/src/main/java/ni/edu/uam/navegacionapp/ui/screens/PerfilScreen.kt
@@ -1,2 +1,68 @@
 package ni.edu.uam.navegacionapp.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun PerfilScreen(navController: NavController) {
+
+    var likes by remember { mutableStateOf(0) }
+    val maxLikes = 100
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text("Perfil", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text("Likes: $likes ❤️")
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                if (likes < maxLikes) likes++
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Dar Like")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedButton(
+            onClick = {
+                if (likes > 0) likes--
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Quitar Like")
+        }
+
+        if (likes == maxLikes) {
+            Text("Llegaste al límite 😅", color = MaterialTheme.colorScheme.error)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("inicio") {
+                    popUpTo("inicio") { inclusive = true }
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Cerrar sesión")
+        }
+    }
+}
